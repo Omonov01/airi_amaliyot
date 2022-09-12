93913deec4722fd49e780d2c4072d1e17b14f178
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2022-09-12 19:06:54 +0500

    raqam_dan_matn.py
    o'n usti 63 gacha text formatiga o'tkiza oladi

--- raqam_dan_matn.py
+++ raqam_dan_matn.py
@@ -78,3 +78,34 @@ def int_to_en(num):
     raise AssertionError('num is too large: %s' % str(num))
 
 print(int_to_en(31))
+
+########################################################################################
+def numToWords(num,join=True):
+    units = ['','bir','ikki','uch','to\'rt','besh','olti','yetti','sakkiz','to\'qqiz']
+    teens = ['','o\'n_bir','o\'n_ikki','o\'n_uch','o\'n_to\'rt','o\'n_besh','o\'n_olti', 'o\'n_yetti','o\'n_sakkiz','o\'n_to\'qqiz']
+    tens = ['','o\'n','yigirma','o\'ttiz','qirq','ellik','oltmish','yetmish', 'sakson','to\'qson']
+    thousands = ['','ming','million','milliard','trillion','kvadrillion','kvintilion','sekstilion','septillion','oktilion', 'nonillion','dekillion','undekillion','duodekillion', 'tredekillion','quattuordekillion','sexdekillion', 'septendekillion','oktodekillion','novemdekillion', 'vigintillion']
+    words = []
+    if num==0: words.append('nol')
+    else:
+        numStr = '%d'%num
+        numStrLen = len(numStr)
+        groups = (numStrLen+2)//3
+        numStr = numStr.zfill(groups*3)
+        for i in range(0,groups*3,3):
+            h,t,u = int(numStr[i]),int(numStr[i+1]),int(numStr[i+2])
+            g = groups-(i//3+1)
+            if h>=1:
+                words.append(units[h])
+                words.append('yuz')
+            if t>1:
+                words.append(tens[t])
+                if u>=1: words.append(units[u])
+            elif t==1:
+                if u>=1: words.append(teens[u])
+                else: words.append(tens[t])
+            else:
+                if u>=1: words.append(units[u])
+            if (g>=1) and ((h+t+u)>0): words.append(thousands[g]+',')
+    if join: return ' '.join(words)
+    return words
