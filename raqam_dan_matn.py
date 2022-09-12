911c341c04b1304c148e1641a996f3aa4fb922f7
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2022-09-12 17:06:23 +0500

    raqam_dan_matn.py
    10 darajasi 12 gacha sonlarni text formatiga o'tizado

--- raqam_dan_matn.py
+++ raqam_dan_matn.py
@@ -33,3 +33,48 @@ def sep_num(text):
             print(word)
 sep_num("salom men sanjar 2000 yilda to'g'ilganman. 12.5 ,13.8")
 
+###################################################################################################
+def int_to_en(num):
+    d = { 0 : 'nol', 1 : 'bir', 2 : 'ikki', 3 : 'uch', 4 : 'to\'rt', 5 : 'besh',
+          6 : 'olti', 7 : 'yetti', 8 : 'sakkiz', 9 : 'to\'qqiz', 10 : 'o\'n',
+          11 : 'o\'n_bir', 12 : 'o\'n_ikki', 13 : 'o\'n_uch', 14 : 'o\'n_to\'rt',
+          15 : 'o\'n_besh', 16 : 'o\'n_olti', 17 : 'o\'n_yetti', 18 : 'o\'n_sakkiz',
+          19 : 'o\'n_to\'qqiz', 20 : 'yigirma',
+          30 : 'o\'ttiz', 40 : 'qirq', 50 : 'ellik', 60 : 'oltmish',
+          70 : 'yetmish', 80 : 'sakson', 90 : 'to\'qson' }
+    k = 1000
+    m = k * 1000
+    b = m * 1000
+    t = b * 1000
+
+    assert(0 <= num)
+
+    if (num < 20):
+        return d[num]
+
+    if (num < 100):
+        if num % 10 == 0: return d[num]
+        else: return d[num // 10 * 10] + ' ' + d[num % 10]
+
+    if (num < k):
+        if num % 100 == 0: return d[num // 100] + ' yuz'
+        else: return d[num // 100] + ' yuz ' + int_to_en(num % 100)
+
+    if (num < m):
+        if num % k == 0: return int_to_en(num // k) + ' ming'
+        else: return int_to_en(num // k) + ' ming, ' + int_to_en(num % k)
+
+    if (num < b):
+        if (num % m) == 0: return int_to_en(num // m) + ' million'
+        else: return int_to_en(num // m) + ' million, ' + int_to_en(num % m)
+
+    if (num < t):
+        if (num % b) == 0: return int_to_en(num // b) + ' milliard'
+        else: return int_to_en(num // b) + ' milliard, ' + int_to_en(num % b)
+
+    if (num % t == 0): return int_to_en(num // t) + ' trillion'
+    else: return int_to_en(num // t) + ' trillion, ' + int_to_en(num % t)
+
+    raise AssertionError('num is too large: %s' % str(num))
+
+print(int_to_en(31))
